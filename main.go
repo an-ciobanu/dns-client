d20507d1073a4300390218bcd3fced4c905ee21a
Andrei Ciobanu <anciobanu@protonmail.com>
2024-11-03 14:06:32 +0200

    Add first unit test

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+const (
+	port       = "53" //port default pentru DNS
+	queryTypeA = 1    //IPv4
+	queryClass = 1    //IN
+	headerSize = 12   //numarul de biti pentru headerul DNS
+)
+
 func main() {
 
 }
 
-func createDNSQuery() {}
+/*
+*	createDNSQuery: preia un domeniu si returneaza queryul DNS (sub forma de biti) sau o eroare
+*		queryul va fi folosit mai departe sa fie trimis la server
+ */
+func createDNSQuery(domain string) []byte {
+	query := make([]byte, headerSize)
+	return query
+}
 
 func sendDNSQuery() {}
 
--- main_test.go
+++ main_test.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
+// test pentru createDNSQuery
 func TestCreateDNSQuery(t *testing.T) {
 
+	domain := "google.com"
+	query := createDNSQuery(domain)
+
+	if len(query) < headerSize {
+		t.Errorf("Length of query is not correct: got %d, expected at least %d", len(query), headerSize)
+	}
+
+	expectedBytes := []byte{7, 'g', 'o', 'o', 'g', 'l', 'e', 3, 'c', 'o', 'm'}
+
+	if !bytes.Contains(query, expectedBytes) {
+		t.Errorf("Query incorrect: got %v, expected %v", query, expectedBytes)
+	}
 }
 
 func TestSendDNSQuery(t *testing.T) {}
